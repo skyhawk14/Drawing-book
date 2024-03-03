85b47c3fc60da56fefe377e9d2c3bc5678046eb3
Mani <30406990+skyhawk14@users.noreply.github.com>
2024-03-03 17:39:27 +0000

    style: Fix formatting issues  and package.json files

---
+++ app/board/_components/room.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import { ClientSideSuspense } from "@liveblocks/react";
+import { RoomProvider } from "@/liveblocks.config";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <RoomProvider id="my-room" initialPresence={{}}>
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
